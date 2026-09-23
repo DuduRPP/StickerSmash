39b3a416686fef317e3aa61823369e8ff96b588b
DuduRPP <dudurpp1@gmail.com>
2026-09-23 01:07:51 -0300

    feat: add home menu button components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/button";
 import ImageViewer from "@/components/image-viewer";
 import { StyleSheet, View } from "react-native";
 
@@ -8,6 +9,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -22,4 +27,8 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
   },
+  footerContainer: {
+    flex: 1/3,
+    alignItems: "center",
+  }
 });
